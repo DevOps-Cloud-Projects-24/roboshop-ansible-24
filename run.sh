c5fc985b5982b8ab556cfc5ff8bc638e93a20bc2
Ismailsfar7 <118299197+Ismailsfar7@users.noreply.github.com>
2024-07-16 16:12:32 -0500

    Ansible playbooks

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 git pull
-ansible-playbook -i "$1".idevops24.online, -e ansible_user-ec2 -e ansible_password=DevOps321 -e role_name="$1" main.yml
+ansible-playbook -i $1.dev.idevops24.online, -e env=dev -e ansible_user-ec2 -e ansible_password=DevOps321 -e role_name=$1 main.yml
