36dfda289bad31203cd3a2e2369915cfeb98b3ba
Ismailsfar7 <118299197+Ismailsfar7@users.noreply.github.com>
2024-07-16 12:48:29 -0500

    Ansible playbooks

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 git pull
-ansible-playbook -i mongo.idevops24.online, -e ansible_user-ec2 -e ansible_password=DevOps321 -e role_name=mongo main.yml
+ansible-playbook -i $1.idevops24.online, -e ansible_user-ec2 -e ansible_password=DevOps321 -e role_name=$1 main.yml
